6866fb52e9aed7a0715a64f028192ffd974e4d82
saifnihad300 <hossainnihad300@gmail.com>
2025-04-30 20:15:09 +0600

    update server to the client

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['https://cover-letter-generator-iota.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
